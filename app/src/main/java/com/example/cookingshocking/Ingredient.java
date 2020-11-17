7f53ff77e13231aa23473f981f29a76e9bbdcc43
DeepikaSharma5 <it19148632@my.sliit.lk>
2020-11-17 11:13:21 +0530

    Created all pages to add recipes

--- app/src/main/java/com/example/cookingshocking/Ingredient.java
+++ app/src/main/java/com/example/cookingshocking/Ingredient.java
@@ -2,13 +2,28 @@ package com.example.cookingshocking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Ingredient extends AppCompatActivity {
+
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient);
+
+        next = findViewById(R.id.button9);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Ingredient.this,Direction.class);
+                startActivity(intent);
+            }
+        });
     }
 }
